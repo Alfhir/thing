351547d0d0122d9df473fa8e18447c0fde2495e0
Oliver <oliver.eisenbarth@gmail.com>
2023-11-06 17:03:21 +0100

    Format and imports

--- src/main/kotlin/com/example/demo/dungeon/internal/Dungeon.kt
+++ src/main/kotlin/com/example/demo/dungeon/internal/Dungeon.kt
@@ -1,11 +1,13 @@
 package com.example.demo.dungeon.internal
 
 import org.jmolecules.ddd.types.AggregateRoot
+import org.jmolecules.ddd.types.Association
 import org.jmolecules.ddd.types.Identifier
 import java.util.*
 
 @JvmInline
 value class DungeonIdentifier(val id: UUID = UUID.randomUUID()): Identifier
 
-data class Dungeon(override val id: DungeonIdentifier = DungeonIdentifier()) :
-    AggregateRoot<Dungeon, DungeonIdentifier>
+data class Dungeon(
+    override val id: DungeonIdentifier = DungeonIdentifier(),
+) : AggregateRoot<Dungeon, DungeonIdentifier>

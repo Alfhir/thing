0a55ad52cbce40b548f36906092a1750c0574c5d
Oliver <oliver.eisenbarth@gmail.com>
2023-11-06 18:16:29 +0100

    Add factory methods to identifiers

--- src/main/kotlin/com/example/demo/dungeon/internal/Dragon.kt
+++ src/main/kotlin/com/example/demo/dungeon/internal/Dragon.kt
@@ -4,8 +4,11 @@ import org.jmolecules.ddd.types.Entity
 import org.jmolecules.ddd.types.Identifier
 import java.util.*
 
+
 @JvmInline
-value class DragonIdentifier(val id: UUID = UUID.randomUUID()): Identifier
+value class DragonIdentifier(val id: UUID = UUID.randomUUID()): Identifier {
+    fun of(id: UUID)  = DragonIdentifier(id)
+}
 
 data class Dragon(override val id: DragonIdentifier = DragonIdentifier(), val color: Color) :
     Entity<Dungeon, DragonIdentifier>
--- src/main/kotlin/com/example/demo/dungeon/internal/Dungeon.kt
+++ src/main/kotlin/com/example/demo/dungeon/internal/Dungeon.kt
@@ -1,12 +1,13 @@
 package com.example.demo.dungeon.internal
 
 import org.jmolecules.ddd.types.AggregateRoot
-import org.jmolecules.ddd.types.Association
 import org.jmolecules.ddd.types.Identifier
 import java.util.*
 
 @JvmInline
-value class DungeonIdentifier(val id: UUID = UUID.randomUUID()): Identifier
+value class DungeonIdentifier(val id: UUID = UUID.randomUUID()): Identifier {
+    fun of(id: UUID)  = DungeonIdentifier(id)
+}
 
 data class Dungeon(
     override val id: DungeonIdentifier = DungeonIdentifier(),

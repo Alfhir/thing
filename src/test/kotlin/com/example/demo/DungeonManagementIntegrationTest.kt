5fc4b529c0d8d0a0606945508d4d309cd64bd693
Oliver <oliver.eisenbarth@gmail.com>
2023-11-08 14:51:55 +0100

    Change test scope

--- src/test/kotlin/com/example/demo/DungeonManagementIntegrationTest.kt
+++ src/test/kotlin/com/example/demo/DungeonManagementIntegrationTest.kt
@@ -5,10 +5,10 @@ import com.example.demo.dungeon.DungeonManagement
 import com.example.demo.dungeon.internal.Color
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.modulith.test.ApplicationModuleTest
 
-// Test only components of one module
-@ApplicationModuleTest
+@SpringBootTest
 class DungeonManagementIntegrationTests {
     @Autowired
     lateinit var gameMaster: DungeonManagement

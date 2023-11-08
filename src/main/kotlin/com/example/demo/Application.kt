0d1a00c5bf56515ca8cf7bbb05e1f37b41852fda
Oliver <oliver.eisenbarth@gmail.com>
2023-11-08 21:42:50 +0100

    Application runner

--- src/main/kotlin/com/example/demo/Application.kt
+++ src/main/kotlin/com/example/demo/Application.kt
@@ -1,15 +1,25 @@
 package com.example.demo
 
+import com.example.demo.dungeon.Dungeons
 import com.example.demo.dungeon.internal.Dungeon
-import com.example.demo.dungeon.internal.Dungeons
+import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-	val applicationContext = runApplication<Application>(*args)
-	val repository = applicationContext.getBean(Dungeons::class.java);
-	val dungeon = repository.save(Dungeon());
+	runApplication<Application>(*args)
+}
+
+@Component
+class ApplicationRunner(private val repository: Dungeons) : org.springframework.boot.ApplicationRunner {
+
+	override fun run(args: ApplicationArguments?) {
+		saveDungeon()
+	}
+
+	private fun saveDungeon() = repository.save(Dungeon()).also(::println)
 }
